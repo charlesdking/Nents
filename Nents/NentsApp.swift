a48b7a5887fcf8856d08d45cecf3fc75d2fa6992
Charles King <charlesdaveking@gmail.com>
2025-12-22 00:08:23 -0600

    Removed all other components leaving only the Primary Button for this branch feature

--- Nents/NentsApp.swift
+++ Nents/NentsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct NentsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(title: "Tap Me", action: { print("Continue tapped") })
         }
     }
 }
