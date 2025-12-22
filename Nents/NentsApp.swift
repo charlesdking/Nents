e554190842536073187232fa60cbbe6f017c16ae
Charles King <charlesdaveking@gmail.com>
2025-12-21 23:06:44 -0600

    Initial Commit

---
+++ Nents/NentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NentsApp.swift
+//  Nents
+//
+//  Created by Charles King on 12/21/25.
+//
+
+import SwiftUI
+
+@main
+struct NentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
